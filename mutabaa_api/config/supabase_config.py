a31aaf866e4a749bd7597c9fc9b326a59771857f
Mutab3a <hhhn1754@gmail.com>
2025-10-13 08:23:07 +0400

    Initial commit
    Initial commit

---
+++ mutabaa_api/config/supabase_config.py
@@ -0,0 +1,33 @@
+"""
+إعدادات الاتصال بـ Supabase
+"""
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+# تحميل المتغيرات البيئية
+load_dotenv()
+
+# معلومات Supabase
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+
+# إنشاء عميل Supabase
+supabase: Client = None
+
+def init_supabase():
+    """تهيئة اتصال Supabase"""
+    global supabase
+    if SUPABASE_URL and SUPABASE_KEY:
+        supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+        return supabase
+    else:
+        raise ValueError("معلومات Supabase غير موجودة في المتغيرات البيئية")
+
+def get_supabase():
+    """الحصول على عميل Supabase"""
+    global supabase
+    if supabase is None:
+        supabase = init_supabase()
+    return supabase
+
